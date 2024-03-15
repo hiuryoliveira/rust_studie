1a4a892b29dea0ea8072bbfcf54f19c385043239
Hiury Joaquim Oliveira André Araújo <hiuryo1996@gmail.com>
2024-03-14 21:52:52 -0300

    Adicionando casos de estudo

--- src/hello_world.rs
+++ src/hello_world.rs
@@ -1,10 +1,5 @@
 
 /// Prints "Hello, world!" to the console.
-///
-/// # Example:
-///
-/// let result = hello_world();
-///
 pub(crate) fn hello_world() {
     println!("Hello, world!");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod hello_world;
+mod guess;
 
 fn main() {
     hello_world::hello_world();
+    guess::guess();
 }
